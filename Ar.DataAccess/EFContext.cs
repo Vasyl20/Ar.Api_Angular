d39793a934c8d8a0cae7baae8b67124bdfb16360
Vasyl20 <57089606+Vasyl20@users.noreply.github.com>
2021-04-12 20:09:41 +0300

    Api

---
+++ Ar.DataAccess/EFContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ar.DataAccess
+{
+    class EFContext
+    {
+    }
+}
---
+++ Ar.DataAccess/Entity/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ar.DataAccess.Entity
+{
+    class User
+    {
+    }
+}
